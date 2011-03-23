2453dda0304bb55b28dc843df8b9f52794f8bc1a
Peter Hyben <hugis@tag.sk>
2011-03-23 23:15:44 +0100

    Create base page template

--- djcode/urls.py
+++ djcode/urls.py
@@ -1,16 +1,12 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^djcode/', include('djcode.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+	(r"^$", direct_to_template, {"template": "index.html"}, "site_home"),
+#	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#	(r'^admin/', include(admin.site.urls)),
 )
