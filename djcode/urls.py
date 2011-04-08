970b49b9c35a8c5616e0f81502b1e97919046858
Peter Hyben <hugis@tag.sk>
2011-04-08 11:21:08 +0200

    Enable the admin in urls

--- djcode/urls.py
+++ djcode/urls.py
@@ -2,17 +2,19 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns("",
 	(r"^$", direct_to_template, {"template": "index.html"}, "site_home"),
-#	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-#	(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
         urlpatterns += patterns("",
                 (r"^media/(?P<path>.*)$", "django.views.static.serve", {"document_root": settings.MEDIA_ROOT}),
+		(r"^admin/doc/", include("django.contrib.admindocs.urls")),
         )
+
+urlpatterns += patterns("",
+	(r"^admin/", include(admin.site.urls)),
+)
