401429786323a936b9dc2fa0f4b7ba0f3bdce3b9
Peter Hyben <hugis@tag.sk>
2011-04-25 21:47:54 +0200

    Front_page view update reservation record if form in request is valid

--- djcode/reservations/views.py
+++ djcode/reservations/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render_to_response, get_object_or_404
+import datetime
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from djcode.reservations.forms import Patient_form
-from djcode.reservations.models import Medical_office
+from djcode.reservations.models import Medical_office, Patient
+from djcode.reservations.models import get_hexdigest
 
 def front_page(request):
-	form = Patient_form()
+	if request.method == 'POST':
+		form = Patient_form(request.POST)
+		if form.is_valid():
+			hexdigest = get_hexdigest(form.cleaned_data["ident_hash"])
+			patient, patient_created = Patient.objects.get_or_create(ident_hash=hexdigest,
+					defaults={
+						"first_name": form.cleaned_data["first_name"],
+						"last_name": form.cleaned_data["last_name"],
+						"ident_hash": form.cleaned_data["ident_hash"],
+						"phone_number": form.cleaned_data["phone_number"],
+						"email": form.cleaned_data["email"],
+					})
+
+			reservation = form.cleaned_data["reservation"]
+			reservation.patient = patient
+			reservation.exam_kind = form.cleaned_data["exam_kind"]
+			reservation.status = 3
+			reservation.booked_at = datetime.datetime.now()
+			reservation.save()
+	else:
+		form = Patient_form()
 	
 	return render_to_response(
 		"index.html",
