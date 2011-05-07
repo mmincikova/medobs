85b2c4101abe4b4ded5439896d4930056bb22912
Peter Hyben <hugis@tag.sk>
2011-05-07 11:07:31 +0200

    Fix #8: added list_filter options to models on admin site configuration

--- djcode/reservations/admin.py
+++ djcode/reservations/admin.py
@@ -2,15 +2,10 @@ from django.contrib import admin
 from djcode.reservations.models import Examination_kind, Medical_office, Office_phone, Patient
 from djcode.reservations.models import Visit_disable_rule, Visit_reservation, Visit_template
 
-admin.site.register(Examination_kind)
-
-class Office_phone_Inline(admin.TabularInline):
-    model = Office_phone
-
-class Medical_office_Admin(admin.ModelAdmin):
-	list_display = ("name", "street", "zip_code", "city", "email", "public")
-	inlines = [Office_phone_Inline,]
-admin.site.register(Medical_office, Medical_office_Admin)
+class Visit_reservation_Admin(admin.ModelAdmin):
+	list_display = ("starting_time", "place", "status")
+	list_filter = ("status", "place", "starting_time")
+admin.site.register(Visit_reservation, Visit_reservation_Admin)
 
 class Patient_Admin(admin.ModelAdmin):
 	list_display = ("full_name", "phone_number", "email", "ident_hash", "has_reservation")
@@ -19,13 +14,21 @@ admin.site.register(Patient, Patient_Admin)
 
 class Visit_template_Admin(admin.ModelAdmin):
 	list_display = ("__unicode__", "place", "valid_since", "valid_until")
+	list_filter = ("place", "day")
 admin.site.register(Visit_template, Visit_template_Admin)
-
-class Visit_reservation_Admin(admin.ModelAdmin):
-	list_display = ("starting_time", "place", "status")
-	list_filter = ("status",)
-admin.site.register(Visit_reservation, Visit_reservation_Admin)
 
 class Visit_disable_rule_Admin(admin.ModelAdmin):
 	list_display = ("begin", "end", "place")
+	list_filter = ("place", "begin")
 admin.site.register(Visit_disable_rule, Visit_disable_rule_Admin)
+
+class Office_phone_Inline(admin.TabularInline):
+    model = Office_phone
+
+class Medical_office_Admin(admin.ModelAdmin):
+	list_display = ("name", "street", "zip_code", "city", "email", "public")
+	inlines = [Office_phone_Inline,]
+	ordering = ["id",]
+admin.site.register(Medical_office, Medical_office_Admin)
+
+admin.site.register(Examination_kind)
