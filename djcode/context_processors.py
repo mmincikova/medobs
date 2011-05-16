e685ff5506ac01cad8952bbff78569e9758b8f0b
Peter Hyben <hugis@tag.sk>
2011-05-16 15:42:42 +0200

    Added settings for load datepicker localization file

--- djcode/context_processors.py
+++ djcode/context_processors.py
@@ -1,7 +1,11 @@
-# -*- coding: utf-8 -*-
+from django.conf import settings
 from djcode.version import get_version
 
 def version(request):
 	return {"VERSION": get_version()}
+
+
+def datepicker_i18n_file(request):
+	return {"DATEPICKER_I18N_FILE": settings.DATEPICKER_I18N_FILE}
 
 # vim: set ts=8 sts=8 sw=8 noet:
