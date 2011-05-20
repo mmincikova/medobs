437e502eb841b7f65d482f405845abd358b732b0
Ivan Mincik <ivan.mincik@gmail.com>
2011-05-20 11:27:25 +0200

    * Adding 'medobstemplates' manage command for automatic generation of 'Visit_template's

---
+++ djcode/reservations/management/commands/medobstemplates.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+""" Automaticaly generate Medobs 'visit template' by selecting start/end times and interval """
+
+import datetime
+
+from django.core.management.base import BaseCommand, CommandError
+from djcode.reservations.models import Medical_office, Visit_template
+
+
+TEMPLATE_VALID_SINCE = '2000-01-01'
+
+def create_visit_template(place, starttime, endtime, interval):
+	intervaltime = datetime.timedelta(minutes=interval)
+
+	templatetime = starttime
+	while templatetime.time() <= endtime.time():
+		for day in Visit_template.DAYS:
+			if day[0] <= 5: # do not create templates for Saturday and Sunday
+				if not Visit_template.objects.filter(place=place, day=day[0], starting_time=templatetime.time()):
+					print 'I: Creating template:  %s %s %s' % (place.name, day[1], templatetime.time())
+					Visit_template.objects.create(place=place, day=day[0], starting_time=templatetime.time(), valid_since=TEMPLATE_VALID_SINCE)
+				else:
+					print 'W: Template already exists:  %s %s %s ... (skipping)' % (place.name, day[1], templatetime.time())
+
+		templatetime = templatetime + intervaltime
+
+class Command(BaseCommand):
+	help = __doc__
+	args = "starttime(HH:MM) endtime(HH:MM) interval(MM)"
+	
+	def handle(self, *args, **options):
+		if len(args) != 3:
+			raise CommandError("Missing some command parameters.")
+		
+		starttime = datetime.datetime.strptime(args[0], '%H:%M')
+		endtime = datetime.datetime.strptime(args[1], '%H:%M')
+		interval = int(args[2])
+		
+		for place in Medical_office.objects.all():
+			create_visit_template(place, starttime, endtime, interval)
+
+# vim: set ts=8 sts=8 sw=8 noet:
